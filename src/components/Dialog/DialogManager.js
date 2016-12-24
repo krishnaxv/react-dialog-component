cd0c6a8dbe47653738a8e168cb3cfa20a0f15f2c
Krishna <krishnaxv@gmail.com>
2016-12-25 03:37:01 +0530

    Alert Dialog component

---
+++ src/components/Dialog/DialogManager.js
@@ -0,0 +1,35 @@
+import React from 'react';  // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom';
+import AlertDialog from './AlertDialogComponent';  // eslint-disable-line no-unused-vars
+import ModalDialog from './ModalDialogComponent';  // eslint-disable-line no-unused-vars
+
+let dialogContainerNode = null;
+let Dialog = null;
+
+const DialogManager = {
+  renderDialog(component) {
+    dialogContainerNode = document.createElement('div');
+    document.body.appendChild(dialogContainerNode);
+    dialogContainerNode.classList.add('dialogContainer');
+    Dialog = ReactDOM.render(component, dialogContainerNode);
+  },
+  showDialog(component) {
+    this.renderDialog(component);
+  },
+  closeDialog(requestFromModule = false) {
+    if (!requestFromModule) {
+      Dialog.removeDialog();
+    }
+    setTimeout(() => {
+      try {
+        ReactDOM.unmountComponentAtNode(dialogContainerNode);
+        document.body.removeChild(dialogContainerNode);
+      } catch (e) {
+        console.error('%c DialogManager.closeDialog() method is invoked after the dialog is destroyed. [Possible Reason] You are passing closeOnAction prop as true and invoking DialogManager.closeDialog() at the same time.', 'color: white; font-weight: bold;');  // eslint-disable-line no-console
+        console.error(e.message);  // eslint-disable-line no-console
+      }
+    }, 700);
+  }
+};
+
+export default DialogManager;
--- src/components/Main/MainComponent.js
+++ src/components/Main/MainComponent.js
@@ -1,9 +1,28 @@
-/* @flow */
+import React, { Component } from 'react';
+import { DialogManager, AlertDialog } from '../Dialog';
 
-import React from 'react';
-
-const Main = () => (
-  <h1>React Dialog Component</h1>
-);
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Just do it!',
+      content: 'I said, "Just do it!". Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ut quaerat itaque minus ab quas ipsum iure consequatur voluptatibus architecto assumenda dolores, eveniet error deleniti iusto iste repudiandae. Beatae, iste?'
+    };
+    this.onReturnDialog = () => {
+      console.log('Dialog is closed.');  // eslint-disable-line no-console
+      // DialogManager.closeDialog();
+    };
+  }
+  onShowDialog() {
+    DialogManager.showDialog(<AlertDialog effect="SIDE_FALL" title={this.state.title} content={this.state.content} returnCallback={() => this.onReturnDialog()} closeOnAction />);
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.onShowDialog()}>Show Dialog</button>
+      </div>
+    );
+  }
+}
 
 export default Main;
