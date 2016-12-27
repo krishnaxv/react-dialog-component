d8f3a3c8a54dea80c6d7a3e1127a54f5d3127096
Krishna <krishnaxv@gmail.com>
2016-12-28 05:03:48 +0530

    Improve dialog rendering with respect to body and user defined parent

--- src/components/Main/MainComponent.js
+++ src/components/Main/MainComponent.js
@@ -15,17 +15,21 @@ const childrenStyle = {
 const effectConfig = {
   start: {
     from: {
+      scale: 0,
       height: 0
     },
     to: {
+      scale: spring(1, presets.wobbly),
       height: spring(320, presets.wobbly)
     }
   },
   end: {
     from: {
+      scale: 1,
       height: 320
     },
     to: {
+      scale: spring(0, presets.noWobble),
       height: spring(0, presets.noWobble)
     }
   }
@@ -33,6 +37,7 @@ const effectConfig = {
 
 const effectReducer = value => (
   {
+    transform: `scale(${value.scale})`,
     height: `${value.height}px`
   }
 );
@@ -51,28 +56,52 @@ class Main extends Component {
   }
   onShowAlertDialog() {
     DialogManager.showDialog(
-      <AlertDialog effect="SIDE_FALL" title={this.state.title} content={this.state.content} returnCallback={() => this.onReturnDialog()} closeOnAction />,
-      this.parent
+      <AlertDialog effect="SIDE_FALL" title={this.state.title} content={this.state.content} returnCallback={() => this.onReturnDialog()} closeOnAction />
     );
   }
   onShowModalDialog() {
+    // DialogManager.showDialog(
+    //   <ModalDialog effectConfig={effectConfig} effectReducer={value => effectReducer(value)} style={style}>
+    //     <div style={childrenStyle}>Modal Dialog Component</div>
+    //   </ModalDialog>,
+    //   this.parent
+    // );
     DialogManager.showDialog(
-      <ModalDialog effectConfig={effectConfig} effectReducer={value => effectReducer(value)} style={style}>
+      <ModalDialog effect="FALL">
         <div style={childrenStyle}>Modal Dialog Component</div>
-      </ModalDialog>
+      </ModalDialog>,
+      this.parent
     );
   }
   render() {
     const parentStyle = {
+      border: '1px solid #f00',
       position: 'relative',
       height: '400px',
-      width: '400px'
+      width: '400px',
+      overflowY: 'auto'
+    };
+    const divStyle = {
+      marginBottom: '8px'
+    };
+    const buttonStyle = {
+      padding: '8px'
     };
     return (
       <div>
-        <button onClick={() => this.onShowAlertDialog()}>Show Alert</button>
-        <button onClick={() => this.onShowModalDialog()}>Show Modal</button>
-        <div ref={(parent) => { this.parent = parent; }} style={parentStyle} />
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('FADE_IN')}>FADE_IN Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('FADE_IN_SCALE')}>FADE_IN_SCALE Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('FALL')}>FALL Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('SIDE_FALL')}>SIDE_FALL Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('SLIDE_IN_TOP')}>SLIDE_IN_TOP Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('SLIDE_IN_RIGHT')}>SLIDE_IN_RIGHT Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('SLIDE_IN_BOTTOM')}>SLIDE_IN_BOTTOM Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('SLIDE_IN_LEFT')}>SLIDE_IN_LEFT Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowAlertDialog('ROTATE_IN')}>ROTATE_IN Effect</button></div>
+        <div style={divStyle}><button style={buttonStyle} onClick={() => this.onShowModalDialog()}>Show Modal</button></div>
+        <div ref={(parent) => { this.parent = parent; }} style={parentStyle}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
     );
   }
